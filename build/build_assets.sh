b349102f81d4712251bda731be32372228b39ef0
YiFei Zhu <zhuyifei1999@gmail.com>
2026-04-24 04:40:14 -0700

    build_assets.sh: Run gimp in xvfb

--- build/build_assets.sh
+++ build/build_assets.sh
@@ -4,13 +4,13 @@ set -ex
 
 REPO="$PWD"
 
-apk add gimp python3 py3-pillow optipng
+apk add xvfb-run gimp python3 py3-pillow optipng
 
 pushd xcf
 
 for DIR in *; do
   echo "$DIR"
-  gimp -n -i -b - <<EOF
+  xvfb-run gimp -n -i -b - <<EOF
   (let* ( (file's (cadr (file-glob "${DIR}/*.xcf" 1))) (filename "") (image 0) (layer 0) )
     (while (pair? file's)
       (set! image (car (gimp-file-load RUN-NONINTERACTIVE (car file's) (car file's))))
